6fd3e7367d6c9840e28a2a66a09233817b473cc5
Parviz Mammadov <65316731+Parviz1989@users.noreply.github.com>
2025-05-15 16:54:49 -0500

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello again  from Azure App Service (Python) 🚀"
+    return "Hello again and again from Azure App Service (Python) 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
